ef24e66d5d631ac867d8f3f256f06e93b4f2d152
Hansikamattaparthi <147487911+Hansikamattaparthi@users.noreply.github.com>
2023-11-06 21:18:25 +0530

    #include<stdio.h> int main() {     int a,b,i;     scanf("%d%d",&a,&b);     i=i>a;     while(i<b)     {         i;        if(i>a&&i<b)        {          printf("%d %d %d ",i,i*i,i*i*i);        }        i++;     } }

---
+++ N_-_S_-_C.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i;
+    scanf("%d%d",&a,&b);
+    i=i>a;
+    while(i<b)
+    {
+        i;
+       if(i>a&&i<b)
+       {
+         printf("%d %d %d
+",i,i*i,i*i*i);
+       }
+       i++;
+    }
+}

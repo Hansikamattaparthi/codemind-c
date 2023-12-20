e1fdefc7f8b306a8cc88a54f6b627b4dacf3a7c1
Hansikamattaparthi <147487911+Hansikamattaparthi@users.noreply.github.com>
2023-12-20 07:10:21 +0530

    #include<stdio.h> int main() {     int n,i,sum=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int se;     scanf("%d",&se);     for(i=0;i<n;i++)     {          if(a[i]==se)          {              printf("True");              break;          }     }     if(a[i]!=se)     {         printf("False");     } }

---
+++ Element_search_in_an_Array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int se;
+    scanf("%d",&se);
+    for(i=0;i<n;i++)
+    {
+         if(a[i]==se)
+         {
+             printf("True");
+             break;
+         }
+    }
+    if(a[i]!=se)
+    {
+        printf("False");
+    }
+}
